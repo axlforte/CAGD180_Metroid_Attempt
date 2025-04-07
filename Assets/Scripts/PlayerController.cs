700248ff3b34f5101f4a32b927452a476ca00ae7
Sky-CAGD <166485290+Sky-CAGD@users.noreply.github.com>
2025-04-07 13:49:11 -0700

    Modified PlayerController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello!");
     }
 
     // Update is called once per frame
