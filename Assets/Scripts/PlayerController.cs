62a959ef5f83c302d97ae205f28cd8f163db375b
GitStomp <alex.lara@cox.net>
2025-04-07 13:48:56 -0700

    made player move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,30 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Check for input to move the player left or right
+    /// </summary>
+    private void Move()
+    {
+        //Check in D or RIght Arrow keys are held
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            GetComponent<Rigidbody>().MovePosition(transform.position + (Vector3.right * speed * Time.deltaTime));
+            //transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        //Check in D or RIght Arrow keys are held
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            GetComponent<Rigidbody>().MovePosition(transform.position + (Vector3.left * speed * Time.deltaTime));
+            //transform.position += Vector3.left * speed * Time.deltaTime;
+        }
     }
 }
