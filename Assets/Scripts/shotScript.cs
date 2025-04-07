a31797de07120f7a795bfa7e48fc32e51dc7de07
axlforte <wisarddlwthecoder@gmail.com>
2025-04-07 13:44:26 -0700

    changed shotscript

--- Assets/Scripts/shotScript.cs
+++ Assets/Scripts/shotScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class shotScript : MonoBehaviour
 {
     public float damage;
+    public int speed;
+    public int dir;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,6 @@ public class shotScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.right * dir * speed * Time.deltaTime;
     }
 }
