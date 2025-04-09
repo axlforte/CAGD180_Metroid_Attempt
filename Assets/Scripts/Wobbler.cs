8d00528a53b05e09d3ce3da0d839bc506bcb0168
axlforte <wisarddlwthecoder@gmail.com>
2025-04-09 11:59:54 -0700

    added enemy script
    enemy script will handle damage, health, and drops if we add drops

--- Assets/Scripts/Wobbler.cs
+++ Assets/Scripts/Wobbler.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Wobbler : MonoBehaviour
+public class Wobbler : enemy
 {
     public int speed;
     public int dir;
     public float health;
+    float wobbleTimer = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,15 @@ public class Wobbler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        wobbleTimer += Time.deltaTime;
         transform.position += Vector3.right * dir * speed * Time.deltaTime;
 
         RaycastHit hit;
 
-        //if(Physics.Raycast(Vector3.Right * dir,))
+        if (!Physics.Raycast(transform.position + new Vector3(0.5f, 0, 0) * dir, Vector3.down, 1) || Physics.Raycast(transform.position + new Vector3(0.5f,0,0) * dir, Vector3.right * dir, 1))
+        {
+            dir *= -1;
+        }
     }
 
     void OnTriggerEnter(Collider other)
